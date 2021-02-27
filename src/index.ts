1c3522e0db083b1b7272fd0220492ff69422a455
Shinigami92 <chrissi92@hotmail.de>
2021-02-27 13:44:04 +0100

    Fix .vue files need to processed for vite dev

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export default (): Plugin => ({
   name: 'vite:ts-nameof',
   enforce: 'pre',
   transform(code, id) {
-    if (id.includes('.ts') && code.includes('nameof')) {
+    if ((id.includes('.ts') || id.includes('.vue')) && code.includes('nameof')) {
       const { fileText, replaced } = tsNameof.replaceInText(id, code);
       if (replaced) {
         code = fileText ?? code;
